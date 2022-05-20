9532591d31e79a40fcac1c3357f77b7a2dc4dc74
Anish Paudel <Rpaudel379@gmail.com>
2022-05-20 12:32:13 +0530

    added carousel in hero section

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
   menuBtn.addEventListener("click", () => {
     mobileMenu.classList.toggle("hide-menu");
-    
-    if(mobileMenu.classList.contains('hide-menu')){
-      menuBtn.innerHTML = '<i class="fa-solid fa-bars"></i>'
-    }else{
-      menuBtn.innerHTML = '<i class="fa-solid fa-close"></i>'
 
+    if (mobileMenu.classList.contains("hide-menu")) {
+      menuBtn.innerHTML = '<i class="fa-solid fa-bars"></i>';
+    } else {
+      menuBtn.innerHTML = '<i class="fa-solid fa-close"></i>';
     }
+  });
 
+  /* swipper js */
+
+  var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
   });
 });
