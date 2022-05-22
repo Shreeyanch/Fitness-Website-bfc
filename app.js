dae0472bbeb0793c7c6257dd91e2fafbde11f98d
blacksheep09 <57945050+blacksheep09@users.noreply.github.com>
2022-05-22 14:40:57 +0530

    fixed services margin issue

--- app.js
+++ app.js
@@ -44,5 +44,17 @@ document.addEventListener("DOMContentLoaded", () => {
     },
 
   });
+
+
+  const top = document.querySelector('.top');
+
+  top.addEventListener('click', ()=>{
+    window.scroll({
+      left:0,
+      top:0,
+      behavior:'smooth'
+    })
+  })
+
 });
 
